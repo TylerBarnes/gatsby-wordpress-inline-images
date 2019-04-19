b5df3315df836cbf13ecf0e0fd4de7ac257b1497
Tyler Barnes <tylerdbarnes@gmail.com>
2019-04-18 22:30:43 -0700

    fix responsive images

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,92 +1,91 @@
-const _ = require(`lodash`);
+const _ = require(`lodash`)
 
-const React = require(`react`);
+const React = require(`react`)
 
-const ReactDOMServer = require(`react-dom/server`);
+const ReactDOMServer = require(`react-dom/server`)
 
-const cheerio = require(`cheerio`);
+const cheerio = require(`cheerio`)
 
-const {
-  createRemoteFileNode
-} = require(`gatsby-source-filesystem`);
+const { createRemoteFileNode } = require(`gatsby-source-filesystem`)
 
-const {
-  fluid
-} = require(`gatsby-plugin-sharp`);
+const { fluid } = require(`gatsby-plugin-sharp`)
 
-const Img = require(`gatsby-image`);
+const Img = require(`gatsby-image`)
 
-const parseWPImagePath = require(`./utils/parseWPImagePath`);
+const parseWPImagePath = require(`./utils/parseWPImagePath`)
 
-exports.sourceNodes = async ({
-  getNodes,
-  cache,
-  reporter,
-  store,
-  actions,
-  createNodeId
-}, pluginOptions) => {
-  const {
-    createNode
-  } = actions;
+exports.sourceNodes = async (
+  { getNodes, cache, reporter, store, actions, createNodeId },
+  pluginOptions
+) => {
+  const { createNode } = actions
   const defaults = {
     maxWidth: 650,
     wrapperStyle: ``,
     backgroundColor: `white`,
     postTypes: ["post", "page"],
-    withWebp: false // linkImagesToOriginal: true,
+    withWebp: false
+    // linkImagesToOriginal: true,
     // showCaptions: false,
     // pathPrefix,
     // withWebp: false
+  }
 
-  };
+  const options = _.defaults(pluginOptions, defaults)
+  const nodes = getNodes()
 
-  const options = _.defaults(pluginOptions, defaults);
+  const entities = nodes.filter(
+    node =>
+      node.internal.owner === "gatsby-source-wordpress" &&
+      options.postTypes.includes(node.type)
+  ) // we need to await transforming all the entities since we may need to get images remotely and generating fluid image data is async
 
-  const nodes = getNodes(); // for now just get all posts and pages.
-  // this will be dynamic later
+  await Promise.all(
+    entities.map(async entity =>
+      transformInlineImagestoStaticImages(
+        {
+          entity,
+          cache,
+          reporter,
+          store,
+          createNode,
+          createNodeId
+        },
+        options
+      )
+    )
+  )
+}
 
-  const entities = nodes.filter(node => node.internal.owner === "gatsby-source-wordpress" && options.postTypes.includes(node.type)); // we need to await transforming all the entities since we may need to get images remotely and generating fluid image data is async
-
-  await Promise.all(entities.map(async entity => transformInlineImagestoStaticImages({
-    entity,
-    cache,
-    reporter,
-    store,
-    createNode,
-    createNodeId
-  }, options)));
-};
-
-const transformInlineImagestoStaticImages = async ({
-  entity,
-  cache,
-  reporter,
-  store,
-  createNode,
-  createNodeId
-}, options) => {
-  const field = entity.content;
-  if (!field && typeof field !== "string" || !field.includes("<img")) return;
-  const $ = cheerio.load(field);
-  const imgs = $(`img`);
-  if (imgs.length === 0) return;
-  let imageRefs = [];
-  imgs.each(function () {
-    imageRefs.push($(this));
-  });
-  await Promise.all(imageRefs.map(thisImg => replaceImage({
-    thisImg,
-    options,
-    cache,
-    reporter,
-    $,
-    store,
-    createNode,
-    createNodeId
-  })));
-  entity.content = $.html();
-};
+const transformInlineImagestoStaticImages = async (
+  { entity, cache, reporter, store, createNode, createNodeId },
+  options
+) => {
+  const field = entity.content
+  if ((!field && typeof field !== "string") || !field.includes("<img")) return
+  const $ = cheerio.load(field)
+  const imgs = $(`img`)
+  if (imgs.length === 0) return
+  let imageRefs = []
+  imgs.each(function() {
+    imageRefs.push($(this))
+  })
+  await Promise.all(
+    imageRefs.map(thisImg =>
+      replaceImage({
+        thisImg,
+        options,
+        cache,
+        reporter,
+        $,
+        store,
+        createNode,
+        createNodeId
+      })
+    )
+  )
+  entity.content = $.html()
+}
 
 const replaceImage = async ({
   thisImg,
@@ -99,28 +98,41 @@ const replaceImage = async ({
   $
 }) => {
   // find the full size image that matches, throw away WP resizes
-  const parsedUrlData = parseWPImagePath(thisImg.attr("src"));
-  const url = parsedUrlData.cleanUrl;
+  const parsedUrlData = parseWPImagePath(thisImg.attr("src"))
+  const url = parsedUrlData.cleanUrl
   const imageNode = await downloadMediaFile({
     url,
     cache,
     store,
     createNode,
     createNodeId
-  });
-  if (!imageNode) return;
-  let classes = thisImg.attr("class");
-  let formattedImgTag = {};
-  formattedImgTag.url = thisImg.attr(`src`);
-  formattedImgTag.classList = classes ? classes.split(" ") : [];
-  formattedImgTag.title = thisImg.attr(`title`);
-  formattedImgTag.alt = thisImg.attr(`alt`);
-  if (parsedUrlData.width) formattedImgTag.width = parsedUrlData.width;
-  if (parsedUrlData.height) formattedImgTag.height = parsedUrlData.height;
-  if (!formattedImgTag.url) return;
-  const fileType = imageNode.ext; // Ignore gifs as we can't process them,
-  // svgs as they are already responsive by definition
+  })
 
+  if (!imageNode) return
+
+  let classes = thisImg.attr("class")
+
+  let formattedImgTag = {
+    url: thisImg.attr(`src`),
+    classList: classes ? classes.split(" ") : [],
+    title: thisImg.attr(`title`),
+    alt: thisImg.attr(`alt`)
+  }
+
+  if (parsedUrlData.width) {
+    formattedImgTag.width = parsedUrlData.width
+  }
+
+  if (parsedUrlData.height) {
+    formattedImgTag.height = parsedUrlData.height
+  }
+
+  if (!formattedImgTag.url) return
+
+  const fileType = imageNode.ext
+
+  // Ignore gifs as we can't process them,
+  // svgs as they are already responsive by definition
   if (fileType !== `gif` && fileType !== `svg`) {
     const rawHTML = await generateImagesAndUpdateNode({
       formattedImgTag,
@@ -129,67 +141,73 @@ const replaceImage = async ({
       cache,
       reporter,
       $
-    }); // Replace the image string
+    })
 
-    if (rawHTML) thisImg.replaceWith(rawHTML);
+    // Replace the image string
+    if (rawHTML) {
+      thisImg.replaceWith(rawHTML)
+    }
   }
-}; // Takes a node and generates the needed images and then returns
+}
+
+// Takes a node and generates the needed images and then returns
 // the needed HTML replacement for the image
-
-
-const generateImagesAndUpdateNode = async function ({
-  formattedImgTag,
+const generateImagesAndUpdateNode = async function({
   imageNode,
   options,
   cache,
-  reporter,
-  $
+  reporter
 }) {
-  if (!imageNode || !imageNode.absolutePath) return;
-  let fluidResultWebp;
-  let fluidResult = await fluid({
+  if (!imageNode || !imageNode.absolutePath) return
+
+  const { maxWidth } = options
+  let fluidResultWebp
+
+  const fluidResult = await fluid({
     file: imageNode,
-    args: { ...options,
-      maxWidth: formattedImgTag.width || options.maxWidth
+    args: {
+      ...options
     },
     reporter,
     cache
-  });
+  })
 
   if (options.withWebp) {
     fluidResultWebp = await fluid({
       file: imageNode,
-      args: { ...options,
-        maxWidth: formattedImgTag.width || options.maxWidth,
-        toFormat: 'WEBP'
+      args: {
+        ...options,
+        toFormat: "WEBP"
       },
       reporter,
       cache
-    });
+    })
   }
 
-  if (!fluidResult) return;
+  if (!fluidResult) return
 
   if (options.withWebp) {
-    fluidResult.srcSetWebp = fluidResultWebp.srcSet;
+    fluidResult.srcSetWebp = fluidResultWebp.srcSet
   }
 
   const imgOptions = {
     fluid: fluidResult,
     style: {
-      maxWidth: "100%"
+      maxWidth,
+      width: "100%"
     },
     // Force show full image instantly
     critical: true,
-    // fadeIn: true,
+    fadeIn: true,
     imgStyle: {
       opacity: 1
     }
-  };
-  if (formattedImgTag.width) imgOptions.style.width = formattedImgTag.width;
-  const ReactImgEl = React.createElement(Img.default, imgOptions, null);
-  return ReactDOMServer.renderToString(ReactImgEl);
-};
+  }
+
+  const ReactImgEl = React.createElement(Img.default, imgOptions, null)
+
+  return ReactDOMServer.renderToString(ReactImgEl)
+}
 
 const downloadMediaFile = async ({
   url,
@@ -208,7 +226,7 @@ const downloadMediaFile = async ({
   // }
   // If we don't have cached data, download the file
   // if (!fileNodeID) {
-  let fileNode = false;
+  let fileNode = false
 
   try {
     fileNode = await createRemoteFileNode({
@@ -217,7 +235,7 @@ const downloadMediaFile = async ({
       cache,
       createNode,
       createNodeId
-    }); // auth: _auth,
+    }) // auth: _auth,
     // if (fileNode) {
     //   fileNodeID = fileNode.id
     //   // await cache.set(mediaDataCacheKey, {
@@ -228,10 +246,9 @@ const downloadMediaFile = async ({
   } catch (e) {} // Ignore
   // }
 
-
-  return fileNode; // if (fileNodeID) {
+  return fileNode // if (fileNodeID) {
   //   e.localFile___NODE = fileNodeID
   //   delete e.media_details.sizes
   // }
   // return e
-};
+}
